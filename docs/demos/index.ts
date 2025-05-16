5f7586fcaae1aa83441786a956821a690a1608c3
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-05-16 15:29:44 +0200

    ✨ Adds presentation demo

--- docs/demos/index.ts
+++ docs/demos/index.ts
@@ -3,6 +3,7 @@ export { configurator } from './Parallax.demo.configurator';
 export { full } from './Parallax.demo.full';
 export { initial } from './Parallax.demo.initial';
 export { parallax } from './Parallax.demo.parallax';
+export { presentation } from './Parallax.demo.presentation';
 export { scroll } from './Parallax.demo.scroll';
 export { shadow } from './Parallax.demo.shadow';
 export { usage } from './Parallax.demo.usage';
