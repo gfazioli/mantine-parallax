673de6644cb6d92344686110feb431ca6bd74330
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-05-16 15:29:44 +0200

    ✨ Adds presentation demo

---
+++ docs/demos/Parallax.demo.presentation.tsx
@@ -0,0 +1,151 @@
+import { Parallax } from '@gfazioli/mantine-parallax';
+import { Badge, Button, Card, Group, Image, Stack, Text } from '@mantine/core';
+import { MantineDemo } from '@mantinex/demo';
+
+function PresentationCard() {
+  const random = Math.floor(Math.random() * 8) + 1;
+
+  const img = `https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-${random}.png`;
+
+  return (
+    <Card
+      shadow="xl"
+      padding="lg"
+      radius="md"
+      withBorder
+      style={{
+        boxShadow: '12px 6px 6px 6px rgba(0, 0, 0, 0.3)',
+      }}
+    >
+      <Card.Section>
+        <Image src={img} height={160} alt="Norway" />
+      </Card.Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
+  );
+}
+
+function Demo() {
+  const presentationProps = {
+    initialRotationX: 60,
+    initialSkewX: -30,
+    initialSkewY: 30,
+    initialPerspective: 10000,
+    disabled: true,
+  };
+
+  return (
+    <Stack w="100%" h={500} align="center">
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 160 }}>
+        <PresentationCard />
+      </Parallax>
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 80 }}>
+        <PresentationCard />
+      </Parallax>
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 0 }}>
+        <PresentationCard />
+      </Parallax>
+    </Stack>
+  );
+}
+
+const code = `
+import { Stack } from '@mantine/core';
+import { Parallax } from '@gfazioli/mantine-parallax';
+import { PresentationCard } from './PresentationCard';
+
+function Demo() {
+  const presentationProps = {
+    initialRotationX: 60,
+    initialSkewX: -30,
+    initialSkewY: 30,
+    initialPerspective: 10000,
+    disabled: true,
+  };
+
+  return (
+    <Stack w="100%" h={500} align="center">
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 160 }}>
+        <PresentationCard />
+      </Parallax>
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 80 }}>
+        <PresentationCard />
+      </Parallax>
+      <Parallax w={400} {...presentationProps} style={{ position: 'absolute', top: 0 }}>
+        <PresentationCard />
+      </Parallax>
+    </Stack>
+  );
+}
+`;
+
+const cardCode = `
+export function PresentationCard() {
+  const random = Math.floor(Math.random() * 8) + 1;
+
+  const img = \`https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-\${random}.png\`;
+
+  return (
+    <Card
+      shadow="xl"
+      padding="lg"
+      radius="md"
+      withBorder
+      style={{
+        boxShadow: '12px 6px 6px 6px rgba(0, 0, 0, 0.3)',
+      }}
+    >
+      <Card.Section>
+        <Image
+          src={img}
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
+  );
+}
+`;
+
+export const presentation: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  code: [
+    {
+      fileName: 'Demo.tsx',
+      code,
+    },
+    {
+      fileName: 'PresentationCard.tsx',
+      code: cardCode,
+    },
+  ],
+  defaultExpanded: false,
+};
